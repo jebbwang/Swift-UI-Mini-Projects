96fb2694a85d7e0fd66ae555220a68d258281d62
Jeff Wang <jeffwang@Jeffs-MacBook-Pro.local>
2025-09-01 22:25:38 -0700

    landmarks

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeff Wang on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
